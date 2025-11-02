b745ebdb653f3ef161bc5fba6759ff90fba05107
LivGish <ST10486576@vcconnect.edu.za>
2025-11-02 12:10:12 +0200

    Menu changed from nav bar to burger style menu

--- js-assets/script.js
+++ js-assets/script.js
@@ -27,3 +27,13 @@ document.addEventListener('DOMContentLoaded', function () {
 		}
 	});
 });
+
+/* Set the width of the sidebar to 250px (show it) */
+function openNav() {
+  document.getElementById("mySidepanel").style.width = "250px";
+}
+
+/* Set the width of the sidebar to 0 (hide it) */
+function closeNav() {
+  document.getElementById("mySidepanel").style.width = "0";
+}
